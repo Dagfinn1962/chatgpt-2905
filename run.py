3048dd65f1d9626ab6ac4dfdc668f28c6a07c1d0
Dagfinn1962 <130903243+Dagfinn1962@users.noreply.github.com>
2023-07-12 18:45:38 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,19 @@
-from server.app     import app
-from server.website import Website
-from server.backend import Backend_Api
+import os
+import openai
 
-from json import load
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-if __name__ == '__main__':
-    config = load(open('config.json', 'r'))
-    site_config = config['site_config']
-    
-    site = Website(app)
-    for route in site.routes:
-        app.add_url_rule(
-            route,
-            view_func = site.routes[route]['function'],
-            methods   = site.routes[route]['methods'],
-        )
-
-    backend_api  = Backend_Api(app, config)
-    for route in backend_api.routes:
-        app.add_url_rule(
-            route,
-            view_func = backend_api.routes[route]['function'],
-            methods   = backend_api.routes[route]['methods'],
-        )
-
-    print(f"Running on port {site_config['port']}")
-    app.run(**site_config)
-    print(f"Closing port {site_config['port']}")
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "user",
+      "content": ""
+    }
+  ],
+  temperature=1,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
